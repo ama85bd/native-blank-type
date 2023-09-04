1471a6a0d0d3e647ce2cb6a8b028831db837a152
Asif M Ashique <asifm.ashique@petronas.com>
2023-09-04 17:16:47 +0600

    success drawer

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import UserScreen from './screens/UserScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName='Home'>
+        <Drawer.Screen name='Home' component={UserScreen} />
+        <Drawer.Screen name='About' component={WelcomeScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
