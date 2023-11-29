3562f0ac97adff7e1e5e5f981fc61e65bacfb891
Asif M Ashique <asifm.ashique@petronas.com>
2023-11-29 18:45:30 +0600

    first try

--- App.tsx
+++ App.tsx
@@ -1,11 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notification from 'expo-notifications';
+import { useEffect } from 'react';
+
+Notification.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  useEffect(() => {
+    const requestNotificationPermission = async () => {
+      const { status } = await Notification.requestPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to receive notifications was denied.');
+      }
+    };
+
+    requestNotificationPermission();
+
+    const subscription1 = Notification.addNotificationReceivedListener(
+      (notification) => {
+        console.log('notification', notification);
+        console.log(notification.request.content.data);
+      }
+    );
+
+    const subscription2 = Notification.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log('response', response);
+      }
+    );
+
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  async function scheduleNotificationHandler() {
+    // Prepare the notification channel
+    await Notification.setNotificationChannelAsync('my-sound', {
+      name: 'default',
+      importance: Notification.AndroidImportance.MAX,
+      sound: '111.mp3', // <- for Android 8.0+, see channelId property below
+    });
+
+    await Notification.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the body of the notification',
+        data: { userName: 'Max' },
+        sound: '111.mp3',
+      },
+      trigger: {
+        seconds: 5,
+        channelId: 'my-sound',
+      },
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title='Schedule Notification'
+        onPress={scheduleNotificationHandler}
+      />
+      <StatusBar style='auto' />
     </View>
   );
 }
